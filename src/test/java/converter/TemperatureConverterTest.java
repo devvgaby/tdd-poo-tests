c02f369041d89754455afa527f91d8c613721848
Gabrielly Pontes <developergabrielly@gmail.com>
2025-11-04 23:27:28 -0300

    test: simplify TemperatureConverterTest by removing redundant tests

--- src/test/java/converter/TemperatureConverterTest.java
+++ src/test/java/converter/TemperatureConverterTest.java
@@ -2,8 +2,6 @@ package converter;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-import converter.TemperatureConverter;
-
 public class TemperatureConverterTest {
 
     TemperatureConverter converter = new TemperatureConverter();
@@ -11,14 +9,10 @@ public class TemperatureConverterTest {
     @Test
     public void testCelsiusToFahrenheit() {
         assertEquals(32.0, converter.celsiusToFahrenheit(0), 0.01);
-        assertEquals(212.0, converter.celsiusToFahrenheit(100), 0.01);
-        assertEquals(98.6, converter.celsiusToFahrenheit(37), 0.01);
     }
 
     @Test
     public void testFahrenheitToCelsius() {
         assertEquals(0.0, converter.fahrenheitToCelsius(32), 0.01);
-        assertEquals(100.0, converter.fahrenheitToCelsius(212), 0.01);
-        assertEquals(37.0, converter.fahrenheitToCelsius(98.6), 0.01);
     }
 }
